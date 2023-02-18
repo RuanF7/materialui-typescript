aacd65499c2cce2e040f85e0395b19a0ddeebf9e
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-18 18:20:40 -0300

    feat: Adicionado responsividade no menu lateral

---
+++ src/shared/contexts/DrawerContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useCallback, useContext, useState } from 'react';
+
+
+
+interface IDrawerContextData {
+  isDrawerOpen: boolean;
+  toggleDrawerOpen: () => void;
+}
+
+interface IDrawerProviderProps {
+  children: React.ReactNode
+}
+
+const DrawerContext = createContext ({} as IDrawerContextData);
+
+export const useDrawerContext = () => {
+  return useContext(DrawerContext);
+};
+
+// eslint-disable-next-line react/prop-types
+export const DrawerProvider: React.FC<IDrawerProviderProps> = ({children}) => {
+  const [isDrawerOpen, setisDrawerOpen] = useState(false);
+ 
+  const toggleDrawerOpen = useCallback(() => {
+    setisDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
+  }, []);
+ 
+  return (
+    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen}}>
+      {children}      
+    </DrawerContext.Provider>
+  );
+};
